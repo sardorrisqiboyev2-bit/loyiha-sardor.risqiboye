641995b0c7efe25367c416869b49c63df0d8535c
sardor.risqiboyev <sardorrisqiboyev2@gmail.com>
2026-05-22 14:22:05 +0500

    main

---
+++ main.js
@@ -0,0 +1,5 @@
+var darkbutton = document.querySelector(".dark");
+var main = document.querySelector(".main");
+darkbutton.addEventListener("click" , function(){
+    main.classList.toggle("dark-mode")
+})
